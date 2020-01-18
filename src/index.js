d821d5820ad4a552c76e4f0c1f599a8a7ea984e2
tundeiness <tundeiness@yahoo.co.uk>
2020-01-18 10:18:04 +0100

    add index,edit App and index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './css/style.css';
+
+
+render(<App />, document.querySelector('#root'));
