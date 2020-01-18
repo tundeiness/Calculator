afecaf056dc7bda417a062d8c66fbf652b2c372c
tundeiness <tundeiness@yahoo.co.uk>
2020-01-18 19:10:30 +0100

    complete milestone-2

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,9 +5,7 @@ import Button from './Button';
 class ButtonPanel extends React.Component{
   render(){
     return(
-      // <div class="button_wrapper">
         <Button onClick={this.onClick}/>
-      // </div>
     );
   }
 }
