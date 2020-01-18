aa3fb10e0d8aaaa8b9bc00cd0a0d9bc83720ed8a
tundeiness <tundeiness@yahoo.co.uk>
2020-01-18 22:18:44 +0100

    edit App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      childDisplay: 0,
-    }
+      childDisplay: '0',
+    };
   }
 
   render() {
     return (
       <div className="app-wrapper">
-        <Display  childDisplay={this.state.childDisplay}/>
-        <ButtonPanel  />
+        <Display childDisplay={this.state.childDisplay} />
+        <ButtonPanel />
       </div>
     );
   }
