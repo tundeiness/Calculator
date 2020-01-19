94a1f601e215cbfde41f56cc4f18197500c50939
tundeiness <tundeiness@yahoo.co.uk>
2020-01-19 23:00:07 +0100

    passed color props to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,33 +9,33 @@ const Button = (props) =>{
   return (
     <div className="calculator_button">
       <div className="groupOne">
-        <button name="AC" className="tierOne" id="clear">AC</button>
-        <button name="+/-" className="tierOne" id="plus_minus">+/-</button>
-        <button name="%" className="tierOne" id="percent">%</button>
-        <button name="/" className="key--operator" id="divide">÷</button>
+        <button name="AC" className="tierOne" id="clear" style={{'color': props.colour}} onClick={props.updateColour}>AC</button>
+        <button name="+/-" className="tierOne" id="plus_minus"  style={{'color': props.colour}} onClick={props.updateColour}>+/-</button>
+        <button name="%" className="tierOne" id="percent"  style={{'color': props.colour}} onClick={props.updateColour}>%</button>
+        <button name="/" className="key--operator" id="divide"  style={{'color': props.colour}} onClick={props.updateColour}>÷</button>
       </div>
       <div className="groupTwo">
-        <button name="7" className="tierOne">7</button>
-        <button name="8" className="tierOne">8</button>
-        <button name="9" className="tierOne">9</button>
-        <button name="x" className="key--operator" id="multiply">&times;</button>
+        <button name="7" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>7</button>
+        <button name="8" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>8</button>
+        <button name="9" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>9</button>
+        <button name="x" className="key--operator" id="multiply"  style={{'color': props.colour}} onClick={props.updateColour}>&times;</button>
       </div>
       <div className="groupThree">
-        <button name="4" className="tierOne">4</button>
-        <button name="5" className="tierOne">5</button>
-        <button name="6" className="tierOne">6</button>
-        <button name="-" className="key--operator" id="minus">-</button>
+        <button name="4" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>4</button>
+        <button name="5" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>5</button>
+        <button name="6" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>6</button>
+        <button name="-" className="key--operator" id="minus"  style={{'color': props.colour}} onClick={props.updateColour}>-</button>
       </div>
       <div className="groupFour">
-        <button name="1" className="tierOne">1</button>
-        <button name="2" className="tierOne">2</button>
-        <button name="3" className="tierOne">3</button>
-        <button name="+" className="key--operator" id="plus">+</button>
+        <button name="1" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>1</button>
+        <button name="2" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>2</button>
+        <button name="3" className="tierOne"  style={{'color': props.colour}} onClick={props.updateColour}>3</button>
+        <button name="+" className="key--operator" id="plus"  style={{'color': props.colour}} onClick={props.updateColour}>+</button>
       </div>
       <div className="groupFive">
-        <button name="0" className="tierTwo">0</button>
-        <button name="." className="tierOne" data-action="decimal">.</button>
-        <button name="=" className="key--equal" id="calculate">=</button>
+        <button name="0" className="tierTwo"  style={{'color': props.colour}} onClick={props.updateColour}>0</button>
+        <button name="." className="tierOne" style={{'color': props.colour}} onClick={props.updateColour}>.</button>
+        <button name="=" className="key--equal" id="calculate"  style={{'color': props.colour}} onClick={props.updateColour}>=</button>
       </div>
     </div>
   );
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,9 +3,22 @@ import React from 'react';
 import Button from './Button';
 
 class ButtonPanel extends React.Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		colour: '#FBAB7E'
+  	}
+  	this.updateColour = this.updateColour.bind(this);
+  }
+
+  updateColour() {
+  	this.setState({
+  		colour: 'red'
+  	});
+  }
   render() {
     return (
-      <Button onClick={this.onClick} />
+      <Button color = {this.state.colour} updateColour={this.updateColour} onClick={this.onClick} />
     );
   }
 }
