ee0bf4c90cad5e8160b8d21dc8b07bbef9c83093
tundeiness <tundeiness@yahoo.co.uk>
2020-01-19 22:27:04 +0100

    change Button structure

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,40 +5,38 @@
 import React from 'react';
 
 
-function Button(props){
-  return (
-    <div className="calculator_button">
-      <div className="groupOne">
-        <button name="AC" className="tierOne" id="clear">AC</button>
-        <button name="+/-" className="tierOne" id="plus_minus">+/-</button>
-        <button name="%" className="tierOne" id="percent">%</button>
-        <button name="/" className="key--operator" id="divide">÷</button>
-      </div>
-      <div className="groupTwo">
-        <button name="7" className="tierOne">7</button>
-        <button name="8" className="tierOne">8</button>
-        <button name="9" className="tierOne">9</button>
-        <button name="x" className="key--operator" id="multiply">&times;</button>
-      </div>
-      <div className="groupThree">
-        <button name="4" className="tierOne">4</button>
-        <button name="5" className="tierOne">5</button>
-        <button name="6" className="tierOne">6</button>
-        <button name="-" className="key--operator" id="minus">-</button>
-      </div>
-      <div className="groupFour">
-        <button name="1" className="tierOne">1</button>
-        <button name="2" className="tierOne">2</button>
-        <button name="3" className="tierOne">3</button>
-        <button name="+" className="key--operator" id="plus">+</button>
-      </div>
-      <div className="groupFive">
-        <button name="0" className="tierTwo">0</button>
-        <button name="." className="tierOne" data-action="decimal">.</button>
-        <button name="=" className="key--equal" id="calculate">=</button>
-      </div>
+const Button = (props) => (
+  <div className="calculator_button">
+    <div className="groupOne">
+      <button name="AC" className="tierOne" id="clear">AC</button>
+      <button name="+/-" className="tierOne" id="plus_minus">+/-</button>
+      <button name="%" className="tierOne" id="percent">%</button>
+      <button name="/" className="key--operator" id="divide">÷</button>
     </div>
-  );
-}
+    <div className="groupTwo">
+      <button name="7" className="tierOne">7</button>
+      <button name="8" className="tierOne">8</button>
+      <button name="9" className="tierOne">9</button>
+      <button name="x" className="key--operator" id="multiply">&times;</button>
+    </div>
+    <div className="groupThree">
+      <button name="4" className="tierOne">4</button>
+      <button name="5" className="tierOne">5</button>
+      <button name="6" className="tierOne">6</button>
+      <button name="-" className="key--operator" id="minus">-</button>
+    </div>
+    <div className="groupFour">
+      <button name="1" className="tierOne">1</button>
+      <button name="2" className="tierOne">2</button>
+      <button name="3" className="tierOne">3</button>
+      <button name="+" className="key--operator" id="plus">+</button>
+    </div>
+    <div className="groupFive">
+      <button name="0" className="tierTwo">0</button>
+      <button name="." className="tierOne" data-action="decimal">.</button>
+      <button name="=" className="key--equal" id="calculate">=</button>
+    </div>
+  </div>
+);
 
 export default Button;
