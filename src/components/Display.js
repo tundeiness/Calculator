03939867bc7b3e4559db9dedf1a3cda20062749d
tundeiness <tundeiness@yahoo.co.uk>
2020-01-21 10:49:47 +0100

    remove braces in Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
   <div className="calculator__display">
