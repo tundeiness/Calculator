5aeb79c13ca05f0fe151e5ad6cf3f22c90344690
tundeiness <tundeiness@yahoo.co.uk>
2020-01-21 14:35:19 +0100

    edit Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,44 +1,38 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable space-before-blocks */
 import React from 'react';
 
 
-const Button = (props) =>{
-  return (
-    <div className="calculator_button">
-      <div className="groupOne">
-        <button name="AC" className="tierOne" id="clear"  onClick={props.updateColour}>AC</button>
-        <button name="+/-" className="tierOne" id="plus_minus"  onClick={props.updateColour}>+/-</button>
-        <button name="%" className="tierOne" id="percent"  onClick={props.updateColour}>%</button>
-        <button name="/" className="key--operator" id="divide"  style={{'color': props.colour}} onClick={props.updateColour}>÷</button>
-      </div>
-      <div className="groupTwo">
-        <button name="7" className="tierOne"  onClick={props.updateColour}>7</button>
-        <button name="8" className="tierOne"   onClick={props.updateColour}>8</button>
-        <button name="9" className="tierOne"   onClick={props.updateColour}>9</button>
-        <button name="x" className="key--operator" id="multiply"  style={{'color': props.colour}} onClick={props.updateColour}>&times;</button>
-      </div>
-      <div className="groupThree">
-        <button name="4" className="tierOne"   onClick={props.updateColour}>4</button>
-        <button name="5" className="tierOne"   onClick={props.updateColour}>5</button>
-        <button name="6" className="tierOne"   onClick={props.updateColour}>6</button>
-        <button name="-" className="key--operator" id="minus"  style={{'color': props.colour}} onClick={props.updateColour}>-</button>
-      </div>
-      <div className="groupFour">
-        <button name="1" className="tierOne"   onClick={props.updateColour}>1</button>
-        <button name="2" className="tierOne"   onClick={props.updateColour}>2</button>
-        <button name="3" className="tierOne"   onClick={props.updateColour}>3</button>
-        <button name="+" className="key--operator" id="plus"  style={{'color': props.colour}} onClick={props.updateColour}>+</button>
-      </div>
-      <div className="groupFive">
-        <button name="0" className="tierTwo"   onClick={props.updateColour}>0</button>
-        <button name="." className="tierOne"  onClick={props.updateColour}>.</button>
-        <button name="=" className="key--equal" id="calculate"  style={{'color': props.colour}} onClick={props.updateColour}>=</button>
-      </div>
+const Button = () => (
+  <div className="calculator_button">
+    <div className="groupOne">
+      <button type="button" name="AC" className="tierOne" id="clear">AC</button>
+      <button type="button" name="+/-" className="tierOne" id="plus_minus">+/-</button>
+      <button type="button" name="%" className="tierOne" id="percent">%</button>
+      <button type="button" name="/" className="key--operator" id="divide">÷</button>
     </div>
-  );
-}
+    <div className="groupTwo">
+      <button type="button" name="7" className="tierOne">7</button>
+      <button type="button" name="8" className="tierOne">8</button>
+      <button type="button" name="9" className="tierOne">9</button>
+      <button type="button" name="x" className="key--operator" id="multiply">&times;</button>
+    </div>
+    <div className="groupThree">
+      <button type="button" name="4" className="tierOne">4</button>
+      <button type="button" name="5" className="tierOne">5</button>
+      <button type="button" name="6" className="tierOne">6</button>
+      <button type="button" name="-" className="key--operator" id="minus">-</button>
+    </div>
+    <div className="groupFour">
+      <button type="button" name="1" className="tierOne">1</button>
+      <button type="button" name="2" className="tierOne">2</button>
+      <button type="button" name="3" className="tierOne">3</button>
+      <button type="button" name="+" className="key--operator" id="plus">+</button>
+    </div>
+    <div className="groupFive">
+      <button type="button" name="0" className="tierTwo">0</button>
+      <button type="button" name="." className="tierOne" data-action="decimal">.</button>
+      <button type="button" name="=" className="key--equal" id="calculate">=</button>
+    </div>
+  </div>
+);
 
 export default Button;
