996dfb19953d6d697bdac9b8fd2bf172a9f29534
tundeiness <tundeiness@yahoo.co.uk>
2020-01-21 10:36:27 +0100

    remove comments in Display.js

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Button from './Button';
 
+
 class ButtonPanel extends React.Component {
   render() {
     return (
@@ -9,5 +10,6 @@ class ButtonPanel extends React.Component {
     );
   }
 }
+
 
 export default ButtonPanel;
