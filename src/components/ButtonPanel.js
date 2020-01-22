02312fbf35bb138698fdeef4506f96036467238f
tundeiness <tundeiness@yahoo.co.uk>
2020-01-22 09:59:21 +0100

    remove render() in ButtonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,9 +2,8 @@
 /* eslint-disable react/no-unused-state */
 
 import React from 'react';
-import Button from './Button';
 import { PropTypes } from 'prop-types';
-
+import Button from './Button';
 
 
 const styles = {
@@ -12,31 +11,22 @@ const styles = {
   textAlign: 'center',
 };
 
-const ButtonPanel = ({name, color, wide}) =>{
-
-  render() {
-    const { color } = this.state;
-    // const { wide } = this.state;
-    return (
-      <Button style={styles} color={color} updateColour={this.updateColour} onClick={this.onClick}>{name}</Button>
-    );
-  }
-
-}
+const ButtonPanel = ({ name, color, wide }) => (
+  <Button style={styles} color={color} updateColour={this.updateColour} onClick={this.onClick}>{name}</Button>
+);
 
 
 ButtonPanel.propTypes = {
   name: PropTypes.string,
   color: PropTypes.string,
   wide: PropTypes.bool,
-}
+};
 
 ButtonPanel.defaultProps = {
   name: 'name',
   color: '#FBAB7E',
-  wide:false
-}
-
+  wide: false,
+};
 
 
 export default ButtonPanel;
