de74c6795109309ac28799d04c5ff4610f8f3523
tundeiness <tundeiness@yahoo.co.uk>
2020-01-22 17:53:11 +0100

    edit Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,38 +1,56 @@
+
+/* eslint-disable max-len */
+/* eslint-disable react/no-unused-state */
+
 import React from 'react';
+import { PropTypes } from 'prop-types';
+// import Button from './ButtonPanel';
 
 
-const Button = () => (
-  <div className="calculator_button">
-    <div className="groupOne">
-      <button type="button" aria-label="AC" name="AC" className="tierOne" id="clear" />
-      <button type="button" aria-label="+/-" name="+/-" className="tierOne" id="plus_minus" />
-      <button type="button" aria-label="%" name="%" className="tierOne" id="percent" />
-      <button type="button" aria-label="/" name="/" className="key--operator" id="divide" />
-    </div>
-    <div className="groupTwo">
-      <button type="button" aria-label="7" name="7" className="tierOne" />
-      <button type="button" aria-label="8" name="8" className="tierOne" />
-      <button type="button" aria-label="9" name="9" className="tierOne" />
-      <button type="button" aria-label="x" name="x" className="key--operator" id="multiply" />
-    </div>
-    <div className="groupThree">
-      <button type="button" aria-label="4" name="4" className="tierOne" />
-      <button type="button" aria-label="5" name="5" className="tierOne" />
-      <button type="button" aria-label="6" name="6" className="tierOne" />
-      <button type="button" aria-label="-" name="-" className="key--operator" id="minus" />
-    </div>
-    <div className="groupFour">
-      <button type="button" aria-label="1" name="1" className="tierOne" />
-      <button type="button" aria-label="2" name="2" className="tierOne" />
-      <button type="button" aria-label="3" name="3" className="tierOne" />
-      <button type="button" aria-label="+" name="+" className="key--operator" id="plus" />
-    </div>
-    <div className="groupFive">
-      <button type="button" aria-label="0" name="0" className="tierTwo" />
-      <button type="button" aria-label="." name="." className="tierOne" data-action="decimal" />
-      <button type="button" aria-label="=" name="=" className="key--equal" id="calculate" />
-    </div>
-  </div>
-);
+// const styles = {
+//   fontFamily: 'sans-serif',
+//   textAlign: 'center',
+//   width: '2rem',
+// };
+
+
+const Button = (props) => {
+  const { name, color, wide } = props;
+
+  let styling = 'tierOne tierTwo';
+
+  if (color !== '#FBAB7E') {
+    styling = 'tierOne';
+  } else {
+    styling = 'key--operator key--equal';
+  }
+
+  if (wide === true) {
+    styling = 'tierTwo';
+  } else {
+    styling = 'tierOne';
+  }
+
+  return (
+    <button type="button" className={styling}>{name}</button>
+  );
+};
+
+// style={{ color: '#FBAB7E' }} style={{backgroundColor: "lightblue"}}
+
+// color: #d9d9d9;
+
+Button.propTypes = {
+  name: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: '#d9d9d9',
+  wide: false,
+  name: 'name',
+};
+
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,32 +1,39 @@
-/* eslint-disable max-len */
-/* eslint-disable react/no-unused-state */
-
 import React from 'react';
-import { PropTypes } from 'prop-types';
 import Button from './Button';
 
 
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
-
-const ButtonPanel = ({ name, color, wide }) => (
-  <Button style={styles} color={color} updateColour={this.updateColour} onClick={this.onClick}>{name}</Button>
+const ButtonPanel = () => (
+  <div className="calculator_button">
+    <div className="groupOne">
+      <Button aria-label="AC" name="AC" className="tierOne" id="clear" />
+      <Button aria-label="+/-" name="+/-" className="tierOne" id="plus_minus" />
+      <Button aria-label="%" name="%" className="tierOne" id="percent" />
+      <Button aria-label="/" name="÷" className="key--operator" id="divide" />
+    </div>
+    <div className="groupTwo">
+      <Button aria-label="7" name="7" className="tierOne" />
+      <Button aria-label="8" name="8" className="tierOne" />
+      <Button aria-label="9" name="9" className="tierOne" />
+      <Button aria-label="x" name="x" className="key--operator" id="multiply" />
+    </div>
+    <div className="groupThree">
+      <Button aria-label="4" name="4" className="tierOne" />
+      <Button aria-label="5" name="5" className="tierOne" />
+      <Button aria-label="6" name="6" className="tierOne" />
+      <Button aria-label="-" name="-" className="key--operator" id="minus" />
+    </div>
+    <div className="groupFour">
+      <Button aria-label="1" name="1" className="tierOne" />
+      <Button aria-label="2" name="2" className="tierOne" />
+      <Button aria-label="3" name="3" className="tierOne" />
+      <Button aria-label="+" name="+" className="key--operator" id="plus" />
+    </div>
+    <div className="groupFive">
+      <Button aria-label="0" name="0" className="tierTwo" />
+      <Button aria-label="." name="." className="tierOne" data-action="decimal" />
+      <Button aria-label="=" name="=" className="key--equal" id="calculate" />
+    </div>
+  </div>
 );
-
-
-ButtonPanel.propTypes = {
-  name: PropTypes.string,
-  color: PropTypes.string,
-  wide: PropTypes.bool,
-};
-
-ButtonPanel.defaultProps = {
-  name: 'name',
-  color: '#FBAB7E',
-  wide: false,
-};
-
 
 export default ButtonPanel;
