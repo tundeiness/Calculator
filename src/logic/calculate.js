c7f0485d724ee9ddccffb2eeb01b22373078530c
tundeiness <tundeiness@yahoo.co.uk>
2020-01-23 16:47:16 +0100

    implement calculate.js

---
+++ src/logic/calculate.js
@@ -0,0 +1,22 @@
+const Calculate = (dataInput, buttonName) => {
+  const { total, next, operation } = dataInput;
+  let copy = { total, next, operation };
+
+
+  if (buttonName === '+/-') {
+    copy.total *= -1;
+    copy.next *= -1;
+    copy = { total, next, operation };
+    return copy;
+  } if (buttonName === 'AC') {
+    copy.total = 0;
+    return copy.total;
+  } if (buttonName === '.' && copy.next !== false) {
+    copy.total = [copy.total, '.', copy.next].join('');
+    return copy.total;
+  }
+  const res = operate(copy.total, copy.next, copy.operation);
+  return res;
+};
+
+export default Calculate;
