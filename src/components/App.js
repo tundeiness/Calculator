61c453777466907d5a1da915b49bad69e97d215f
tundeiness <tundeiness@yahoo.co.uk>
2020-01-27 09:02:09 +0100

    remove operation

--- src/components/App.js
+++ src/components/App.js
@@ -27,8 +27,8 @@ class App extends React.Component {
   }
 
   render() {
-    const { total, next, operation } = this.state;
-    const res = next || operation || total;
+    const { total, next } = this.state;
+    const res = next || total;
     if (res) {
       return (
         <div className="app-wrapper">
